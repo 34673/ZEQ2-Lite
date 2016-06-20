c453662d6d241c76034fa238ef7fb6c54a3dd86a
34673 <retrogamerfra@gmail.com>
2016-06-20 20:43:37 +0200

    /*cURL*/
    -- Up to date (see version in respective folder)
    -- Renamed 'libcurl-7.35.0' to 'LibCurl'

--- Engine/client/cl_curl.h
+++ Engine/client/cl_curl.h
@@ -28,9 +28,9 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include "../../Shared/qcommon.h"
 
 #ifdef USE_LOCAL_HEADERS
-  #include "../libcurl-7.35.0/curl/curl.h"
+  #include "../LibCurl/curl.h"
 #else
-  #include <curl/curl.h>
+  #include <curl.h>
 #endif
 
 #ifdef USE_CURL_DLOPEN

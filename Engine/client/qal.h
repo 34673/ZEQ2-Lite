61fa11c2a2ebdfad0513209cc1fa6aebd89e3f75
34673 <retrogamerfra@gmail.com>
2016-06-20 22:30:00 +0200

    /*OpenAL*/
    -- Up to date (see version in respective folder)
    -- Renamed 'AL' folder to 'OpenAL'

--- Engine/client/qal.h
+++ Engine/client/qal.h
@@ -34,16 +34,16 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #endif
 
 #ifdef USE_LOCAL_HEADERS
-#include "../AL/al.h"
-#include "../AL/alc.h"
+#include "../OpenAL/al.h"
+#include "../OpenAL/alc.h"
 #else
 #ifdef _MSC_VER
   // MSVC users must install the OpenAL SDK which doesn't use the AL/*.h scheme.
   #include <al.h>
   #include <alc.h>
 #else
-  #include <AL/al.h>
-  #include <AL/alc.h>
+  #include <OpenAL/al.h>
+  #include <OpenAL/alc.h>
 #endif
 #endif
 

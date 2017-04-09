a83166867d0f19b10c2213b504645d357eccee30
34673 <retrogamerfra@gmail.com>
2017-04-09 17:57:58 +0200

    /*CGame*/ -- CG_Public first compacting pass (theoretically successful).

--- Game/CGame/cg_public.h
+++ Game/CGame/cg_public.h
@@ -19,56 +19,43 @@ along with Quake III Arena source code; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 ===========================================================================
 */
-//
-
-
-#define	CMD_BACKUP			64	
-#define	CMD_MASK			(CMD_BACKUP - 1)
+#define	CMD_BACKUP	64	
+#define	CMD_MASK	(CMD_BACKUP-1)
 // allow a lot of command backups for very fast systems
 // multiple commands may be combined into a single packet, so this
 // needs to be larger than PACKET_BACKUP
-
-
 #define	MAX_ENTITIES_IN_SNAPSHOT	256
-
 // snapshots are a view of the server at a given time
-
 // Snapshots are generated at regular time intervals by the server,
 // but they may not be sent if a client's rate level is exceeded, or
 // they may be dropped by the network.
-typedef struct {
-	int				snapFlags;			// SNAPFLAG_RATE_DELAYED, etc
+typedef struct{
+	// SNAPFLAG_RATE_DELAYED, etc
+	int				snapFlags;
 	int				ping;
-
-	int				serverTime;		// server time the message is valid for (in msec)
-
-	byte			areamask[MAX_MAP_AREA_BYTES];		// portalarea visibility bits
-
-	playerState_t	ps;						// complete information about the current player at this time
-
-	int				numEntities;			// all of the entities that need to be presented
-	entityState_t	entities[MAX_ENTITIES_IN_SNAPSHOT];	// at the time of this snapshot
-
-	int				numServerCommands;		// text based server commands to execute when this
-	int				serverCommandSequence;	// snapshot becomes current
+	// server time the message is valid for (in msec)
+	int				serverTime;
+	// portalarea visibility bits
+	byte			areamask[MAX_MAP_AREA_BYTES];
+	// complete information about the current player at this time
+	playerState_t	ps;
+	// all of the entities that need to be presented
+	int				numEntities;
+	// at the time of this snapshot
+	entityState_t	entities[MAX_ENTITIES_IN_SNAPSHOT];
+	// text based server commands to execute when this
+	int				numServerCommands;
+	// snapshot becomes current
+	int				serverCommandSequence;
 } snapshot_t;
-
-enum {
+enum{
   CGAME_EVENT_NONE,
   CGAME_EVENT_EDITHUD,
 };
-
-
-/*
-==================================================================
-
-functions imported from the main executable
-
-==================================================================
-*/
-
+//===============================================
+//functions imported from the main executable
+//===============================================
 #define	CGAME_IMPORT_API_VERSION	4
-
 typedef enum {
 	CG_PRINT,
 	CG_ERROR,
@@ -152,7 +139,6 @@ typedef enum {
 	CG_R_REMAP_SHADER,
 	CG_S_ADDREALLOOPINGSOUND,
 	CG_S_STOPLOOPINGSOUND,
-
 	CG_CM_TEMPCAPSULEMODEL,
 	CG_CM_CAPSULETRACE,
 	CG_CM_TRANSFORMEDCAPSULETRACE,
@@ -162,13 +148,11 @@ typedef enum {
 	CG_R_INPVS,
 	// 1.32
 	CG_FS_SEEK,
-
 /*
 	CG_LOADCAMERA,
 	CG_STARTCAMERA,
 	CG_GETCAMERAINFO,
 */
-
 	CG_MEMSET = 100,
 	CG_MEMCPY,
 	CG_STRNCPY,
@@ -185,56 +169,40 @@ typedef enum {
 	CG_FS_GETFILELIST,
 	CG_R_ADDFOGTOSCENE,
 	// -->
-} cgameImport_t;
-
-
-/*
-==================================================================
-
-functions exported to the main executable
-
-==================================================================
-*/
-
-typedef enum {
+}cgameImport_t;
+//============================================
+//functions exported to the main executable
+//============================================
+typedef enum{
 	CG_INIT,
-//	void CG_Init( int serverMessageNum, int serverCommandSequence, int clientNum )
+	//void CG_Init(int serverMessageNum, int serverCommandSequence, int clientNum)
 	// called when the level loads or when the renderer is restarted
 	// all media should be registered at this time
 	// cgame will display loading status by calling SCR_Update, which
 	// will call CG_DrawInformation during the loading process
 	// reliableCommandSequence will be 0 on fresh loads, but higher for
 	// demos, tourney restarts, or vid_restarts
-
 	CG_SHUTDOWN,
-//	void (*CG_Shutdown)( void );
+	//void (*CG_Shutdown)(void);
 	// oportunity to flush and close any open files
-
 	CG_CONSOLE_COMMAND,
-//	qboolean (*CG_ConsoleCommand)( void );
+	//qboolean (*CG_ConsoleCommand)(void);
 	// a console command has been issued locally that is not recognized by the
 	// main game system.
 	// use Cmd_Argc() / Cmd_Argv() to read the command, return qfalse if the
 	// command is not known to the game
-
 	CG_DRAW_ACTIVE_FRAME,
-//	void (*CG_DrawActiveFrame)( int serverTime, stereoFrame_t stereoView, qboolean demoPlayback );
+	//void (*CG_DrawActiveFrame)(int serverTime, stereoFrame_t stereoView, qboolean demoPlayback);
 	// Generates and draws a game scene and status information at the given time.
 	// If demoPlayback is set, local movement prediction will not be enabled
-
 	CG_CROSSHAIR_PLAYER,
-//	int (*CG_CrosshairPlayer)( void );
-
+	//int (*CG_CrosshairPlayer)(void);
 	CG_LAST_ATTACKER,
-//	int (*CG_LastAttacker)( void );
-
+	//int (*CG_LastAttacker)(void);
 	CG_KEY_EVENT, 
-//	void	(*CG_KeyEvent)( int key, qboolean down );
-
+	//void (*CG_KeyEvent)(int key, qboolean down);
 	CG_MOUSE_EVENT,
-//	void	(*CG_MouseEvent)( int dx, int dy );
+	//void	(*CG_MouseEvent)(int dx, int dy);
 	CG_EVENT_HANDLING
-//	void (*CG_EventHandling)(int type);
-} cgameExport_t;
-
-//----------------------------------------------
+	//void (*CG_EventHandling)(int type);
+}cgameExport_t;

1f5ebc602a978e606270c44645d0801a5ec4ed26
34673 <retrogamerfra@gmail.com>
2017-04-09 19:12:27 +0200

    /*Renderer*/ -- Compile fix

--- Engine/renderer/tr_mesh.c
+++ Engine/renderer/tr_mesh.c
@@ -292,9 +292,8 @@ void R_AddMD3Surfaces( trRefEntity_t *ent ) {
 		|| (ent->e.frame < 0)
 		|| (ent->e.oldframe >= tr.currentModel->md3[0]->numFrames)
 		|| (ent->e.oldframe < 0) ) {
-			//ri.Printf( PRINT_DEVELOPER, "R_AddMD3Surfaces: no such frame %d to %d for '%s'\n",
-				ent->e.oldframe, ent->e.frame,
-				tr.currentModel->name );
+			//Eagle: commenting this out because it is flooding the console. Either the messages should be collapsed into one, or wait for IQM to be ready for use.
+			//ri.Printf( PRINT_DEVELOPER, "R_AddMD3Surfaces: no such frame %d to %d for '%s'\n", ent->e.oldframe, ent->e.frame, tr.currentModel->name );
 			ent->e.frame = 0;
 			ent->e.oldframe = 0;
 	}
